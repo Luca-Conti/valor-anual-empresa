ce5c82e06d14aa8b2b008f6ba0ccc357dd54525e
luca-conti <lucagopro12@gmail.com>
2025-05-19 20:50:03 -0300

    faturamento anual pronto

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from colorama import fore
-print("""
+from colorama import Fore
+import os
+
+def titulo(texto=''):
+    os.system('cls')
+    print(texto)
+    print('')
+
+titulo(texto="""
 ███╗░░░███╗███████╗██████╗░██╗░█████╗░  ░█████╗░███╗░░██╗██╗░░░██╗░█████╗░██╗░░░░░  ██████╗░███████╗
 ████╗░████║██╔════╝██╔══██╗██║██╔══██╗  ██╔══██╗████╗░██║██║░░░██║██╔══██╗██║░░░░░  ██╔══██╗██╔════╝
 ██╔████╔██║█████╗░░██║░░██║██║███████║  ███████║██╔██╗██║██║░░░██║███████║██║░░░░░  ██║░░██║█████╗░░
@@ -15,7 +22,22 @@ print("""
 ╚══════╝╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═╝░░╚═╝╚═════╝░""")
 
 def escolha():
-    escolha_tipo = int(input('escolha 1 para anual e 2 para mensal e 3 para sair'))
+    escolha_tipo = int(input('escolha 1 para anual e 2 para mensal e 3 para sair: '))
     if escolha_tipo == 1:
+        anual()
 
 def anual():
+    titulo(texto='FATURAMNETO ANUAL')
+    quantidade_anos = int(input('quantos anos vc vai analizar: '))
+    faturamentos = []
+
+    for i in range(1, quantidade_anos + 1):
+        faturamento = float(input('qual foi o faturamento anual: '))
+        faturamentos.append(faturamento)
+
+    soma = sum(faturamentos)
+    media = soma / quantidade_anos
+    print(f'o faturamento da {quantidade_anos} foi {media} e o total foi {soma}')
+
+
+escolha()
